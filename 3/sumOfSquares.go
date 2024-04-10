81e33937aa41bbd347b42d263494bd87123c7c94
segadron <segadron199@gmail.com>
2024-04-10 16:58:34 +0300

    create sumOfSquares

---
+++ 3/sumOfSquares.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	numbers := []int{2, 4, 6, 8, 10}
+
+	var wg sync.WaitGroup
+	wg.Add(len(numbers))
+
+	sumSquares := make(chan int)
+
+	for _, num := range numbers {
+		go func(n int) {
+			defer wg.Done()
+			sumSquares <- n * n
+		}(num)
+	}
+
+	go func() {
+		wg.Wait()
+		close(sumSquares)
+	}()
+
+	total := 0
+	for square := range sumSquares {
+		total += square
+	}
+
+	fmt.Println("Sum of squares:", total)
+}
